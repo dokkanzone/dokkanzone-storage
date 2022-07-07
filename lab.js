c36764cffa0a5b0c4a7827319414f1e83b999952
dokkanzone <dokkanzone@proton.me>
2022-07-07 16:03:59 +0200

    2022-07-07

--- lab.js
+++
@@ -1,8 +0,0 @@
-import { readdirSync } from "fs";
-
-const dirs = readdirSync("./assets/cards");
-
-for (const dir of dirs) {
-  const files = readdirSync(`./assets/cards/${dir}`);
-  if (files.length < 4) console.log(dir, files);
-}
